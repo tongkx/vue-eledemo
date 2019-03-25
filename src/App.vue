292a5171f44a2971049208c04d8dd7182a77e6d0
tongkx <3336097126@qq.com>
2019-03-25 18:53:31 +0800

    商品页

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-header :seller = seller></v-header>
+    <ul class="tab">
+      <li class="tab-item">商品</li>
+      <li>评价</li>
+      <li>商家</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import header from './components/header/header'
+const ERR_OK = 0
+export default {
+  name: 'App',
+  components: {
+    'v-header': header
+  },
+  data (){
+    return {
+      seller: []
+    }
+  },
+  created (){
+    axios.get('/api/seller?id=' + this.seller.id).then((respsonse) => {
+      console.info(respsonse.data)
+      respsonse = respsonse.data
+      if(respsonse.errno === ERR_OK){
+        this.seller = respsonse.data
+      }
+    })
+  }
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  @import "./common/sass/index.scss";
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar"><img :src="seller.avatar"></div>
+      <div class="content">
+          <div class="title"><span class="brand"></span><span class="name">{{seller.name}}</span></div>
+          <div class="desc">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
+          <div class="support">
+            <span class="icon"></span>
+            <span class="text">{{seller.supports[0].description}}</span>
+          </div>
+      </div>
+      <div class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+      </div>
+    </div>
+    <div class="bulletin-wrapper"><span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i></div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['seller']
+  }
+</script>
+
+<style lang="scss" scoped rel="stylesheet/scss">
+  .header{
+    color: #fff;
+    position: relative;
+    overflow: hidden;
+  }
+  .background{
+    position: absolute;
+    top: 0;
+    z-index: -1;
+    width: 100%;
+    filter: blur(10px);
+  }
+  .content-wrapper{
+    position: relative;
+    z-index: 2;
+    display: flex;
+    padding: 24px 12px 18px 24px;
+    background: rgba(0,0,0,0.5);
+    .avatar{
+      width: 64px;
+      height: 64px;
+    }
+    .support-count{
+      margin-top: 40px;
+      padding: 0 12px;
+      height: 24px;
+      line-height: 24px;
+      font-size: 12px;
+      background: rgba(0,0,0,0.2);
+      border-radius:15px;
+    }
+  }
+  .content{
+    flex: 1;
+    padding-left: 16px;
+    .title{
+      .name{
+        font-size: 16px;
+      }
+    }
+    .desc{
+      padding: 8px 0 6px;
+      font-size: 12px;
+    }
+    .support{
+      .text{
+        font-size: 10px;
+      }
+    }
+  }
+  .bulletin-wrapper{
+    display: flex;
+    align-items: center;
+    position: relative;
+    z-index: 2;
+    height: 28px;
+    padding: 0 4px;
+    background: rgba(7,17,27,0.5);
+    .bulletin-text{
+      flex: 1;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      font-size: 10px;
+    }
+  }
+</style>
